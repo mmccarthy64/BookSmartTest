a31ab806a44b91d44feab0fa14af621541b2ba4b
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-04 15:51:30 -0600

    update book, user associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     has_many :comments
     belongs_to :genre
-    has_many :users, :through => :genres
+    has_many :users, :through => :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ApplicationRecord
   end
 
   has_many :comments
-  has_many :books, :through => :genres
+  has_many :books, :through => :comments
 end
